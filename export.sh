a2bf26c76916e07fd9c593e84fbfb30fad989807
Derek Weitzel <dweitzel@cse.unl.edu>
2016-08-01 21:22:44 +0000

    Changing test data structure

--- export.sh
+++ export.sh
@@ -2,18 +2,25 @@
 
 date=$1
 
-mkdir gracc.osg.raw-$date
+mkdir gracc.osg.raw0-$date
 
 tmp_file=`mktemp -u`
-elasticdump --input http://127.0.0.1:9200/gracc.osg.raw-$date --output $tmp_file --type=data &
+elasticdump --input http://127.0.0.1:9200/gracc.osg.raw0-$date --output $tmp_file --type=data &
 data_pid=$!
 # Kill the data dump after 2 seconds, then take only the head
-sleep 2
+sleep 5
 kill $data_pid
 wait $data_pid
-head -100 $tmp_file > ./gracc.osg.raw-$date/data.json
+head -1000 $tmp_file > ./gracc.osg.raw0-$date/data.json
 rm -rf $tmp_file
 
-elasticdump --input http://127.0.0.1:9200/gracc.osg.raw-$date --output ./gracc.osg.raw-$date/mapping.json --type=mapping
-elasticdump --input http://127.0.0.1:9200/gracc.osg.raw-$date --output ./gracc.osg.raw-$date/analyzer.json --type=analyzer
+
+elasticdump --input http://127.0.0.1:9200/gracc.osg.raw0-$date --output ./gracc.osg.raw0-$date/mapping.json --type=mapping
+elasticdump --input http://127.0.0.1:9200/gracc.osg.raw0-$date --output ./gracc.osg.raw0-$date/analyzer.json --type=analyzer
+
+tar czf ./gracc.osg.raw0-$date.tar.gz ./gracc.osg.raw0-$date/
+
+
+rm -rf ./gracc.osg.raw0-$date
+
 
--- import.sh
+++ import.sh
@@ -3,6 +3,8 @@
 # Expects elasticdump in the PATH
 
 for dir in `ls | grep gracc`; do
+    tar xzf $dir
+    dir=$(basename $dir .tar.gz)
     elasticdump --input $dir/analyzer.json --output http://localhost:9200/$dir --type=analyzer
     elasticdump --input $dir/mapping.json --output http://localhost:9200/$dir --type=mapping
     elasticdump --input $dir/data.json --output http://localhost:9200/$dir --type=data
